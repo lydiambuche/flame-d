95e2a0308641e2759c9105f4db81205cca399203
lydiambuche <lydiambuche@gmail.com>
2025-03-28 14:02:49 +0300

    week2 assignment

---
+++ index.py
@@ -0,0 +1,7 @@
+num1 = float (input("5.6"))
+num2 = float (input( "4.2"))
+sum_result  = num1 + num2
+difference_result = num1 - num2
+product_result = num1 * num2
+quotient_result = num1 / num2
+print(f"results of your two numbers" )
---
+++ week2/assignment2.py
@@ -0,0 +1,40 @@
+my_list = []
+
+
+
+my_list = []  
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+print(my_list)
+
+
+
+
+my_list = [10, 15, 20, 30, 40] 
+my_list.extend([50, 60, 70])     
+
+print(my_list)
+
+
+
+
+
+my_list = [10, 15, 20, 30, 40, 50, 60, 70]  
+del my_list[-1]
+
+print(my_list)
+
+
+
+
+my_list = [10, 15, 20, 30, 40, 50, 60]
+index_of_30 = my_list.index(30)
+
+print("Index of 30:", index_of_30) 
+
+
+
